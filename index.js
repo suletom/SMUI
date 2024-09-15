3082f3adc0c04502c2945620769e3454941b5bec
sulekata1 <sule.nagy.katalin@gmail.com>
2024-09-15 21:09:58 +0200

    test server

---
+++ index.js
@@ -0,0 +1,62 @@
+var express = require('express');
+const bodyParser = require('body-parser');
+let fs = require('fs');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+let t = "";
+
+try {
+    t = fs.readFileSync('template.html', { encoding: 'utf8', flag: 'r' });
+} catch (e) {
+    console.log(e);
+}
+
+
+app.get('/', function (req, res) {
+
+    let sc = "";
+
+    try {
+        sc = fs.readFileSync('dash.html', { encoding: 'utf8', flag: 'r' });
+    } catch (e) {
+        console.log(e);
+    }
+
+    let d = t;
+    d.replace("<!--sc-->", sc);
+    res.send(d);
+
+});
+
+app.get('/ajax', function (req, res) {
+
+    let d = "";
+
+    res.send(d);
+
+});
+
+app.get('/setup', function (req, res) {
+
+    let sc = "";
+
+    try {
+        sc = fs.readFileSync('setup.html', { encoding: 'utf8', flag: 'r' });
+    } catch (e) {
+        console.log(e);
+    }
+
+    let d = t;
+    d.replace("<!--sc-->", sc);
+    res.send(d);
+
+});
+
+app.listen(6789, function () {
+
+    console.log("Web ui running on port 6789! Access: http://localhost");
+
+});
